ea6f653207ed02f93c14a2caf50f8caf38f343b4
Batyr <68106895+LilBtr@users.noreply.github.com>
2023-08-22 16:53:46 +0300

    Update app.js

--- 10.ifelse/app.js
+++ 10.ifelse/app.js
@@ -2,3 +2,16 @@ const age = 18
 let accessAllowed = age >= 18 // true/false
 
 console.log(accessAllowed)
+
+
+let result
+
+if (2 + 4 < 7) {
+	result = 'Мало'
+} else {
+	result = 'Много'
+}
+
+let result1 = 2 + 4 < 7 ? 'Мало' : 'Много'
+
+console.log(result1)
